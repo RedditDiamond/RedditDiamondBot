54f1c1ea53044e8043809ac507357d45cc8466e0
cmcjacob <39151930+cmcjacob@users.noreply.github.com>
2018-06-17 21:10:51 -0500

    Add files via upload
    no longer using flask for api

--- main.py
+++ main.py
@@ -3,17 +3,13 @@ import praw
 import requests
 import json
 import formatter
-import os
-from flask import Flask
-from flask import request
-from flask import abort
-from flask import jsonify
-from flask import json
-from flask_cors import CORS
 from bs4 import BeautifulSoup
 import config
+import collections
+
 import random
 from random import randint
+
 
 charities = [   "DonorsChoose.org",
                 "St. Jude Children's Hospital",
@@ -36,20 +32,68 @@ charities = [   "DonorsChoose.org",
 
 
 
-app = Flask(__name__)
-CORS(app)
+global base
+global reddit
+base = firebase.FireBase()
+reddit = praw.Reddit(client_id=config.CLIENT_ID, client_secret=config.SECRET,
+                     user_agent="py:redditdiamondbot.com.reddit:v1.0 (by /u/deathfaith, /u/PatrioTech, and /u/cmcjacob)",
+                     username=config.USERNAME,password=config.PASSWORD)
 
-subreddit_black_list = []
+def api_handler(message):
+    path = message["path"]
+    donewithpath = False
+    if 'put' in message["event"]:
+        api_proc = message["data"]
+        print(api_proc)
+        if api_proc is not None:
+            for call,args in api_proc.items():
+                #print("ARGS: " + str(args))
+                if 'poll' in call:
+                    print("API: Detected poll. Processing")
+                    process_new_comments()
+                    donewithpath = True
+                elif 'validate' in call:
+                    print("API: Detected validate. Processing")
+                    if args is not None:
+                        find_space = args.find(' ')
+                        if find_space > -1:
+                            split_args = args.split(' ')
+                            code = split_args[0]
+                            transaction = split_args[1]
+                            donator = split_args[2]
+                            amount = 0
+                            charity = 'None'
+                            if code is not None and transaction is not None and donator is not None:
+                                if 'override' in transaction:
+                                    amount = randint(1, 10)
+                                    charity = random.choice(charities)
+                                    print("API: Validation override, updating database..")
+                                    owner = diamondSuccess_API(amount, donator, code, transaction, charity)
+                                    print("API: Database updated for owner:" + owner)
+                                    donewithpath = True
+                                else:
+                                    amount, charity = get_receipt_info(transaction)
+                                    if amount > 0:
+                                        print("API: Validation succeeded, updating database..")
+                                        owner = diamondSuccess_API(amount, donator, code, transaction, charity)
+                                        print("API: Database updated for owner:" + owner)
+                                        donewithpath = True
+                                    else:
+                                        print("API: Validation failed. Transaction '" + transaction + "' could not be parsed.")
+                            else:
+                                print("API: Failed to retrieve arguments for 'validate' call.")
+    if donewithpath is True:
+        print("Done processing. This call will now be deleted.")
+        base.db.child("api").child(path).remove(base.usertoken)
+        print("Call deleted, API continuing processing.")
 
-with app.app_context():
-    global base
-    global reddit
-    base = firebase.FireBase()
-    reddit = praw.Reddit(client_id=os.environ["REDDIT_CLIENTID"],
-client_secret=os.environ["REDDIT_SECRET"],
-user_agent="py:redditdiamondbot.com.reddit:v1.0 (by /u/deathfaith, /u/PatrioTech, and /u/cmcjacob)",
-username=os.environ["REDDIT_USERNAME"],
-password=os.environ["REDDIT_PASSWORD"])
+
+
+
+print("Starting API handler for table 'api...")
+api_stream = base.db.child("api").stream(api_handler)
+
+
 
 def get_receipt_info(url):
     try:
@@ -65,9 +109,9 @@ def get_receipt_info(url):
     except:
         return -1
 
-def stream_comments():
-    print("stream_comments() called")
-    fire_response = requests.get('https://api.pushshift.io/reddit/search/comment/?q=!redditdiamond').text
+def process_new_comments():
+    print("process_new_comments() called")
+    fire_response = requests.get('https://api.pushshift.io/reddit/search/comment/?q=!testdiamond').text
     fire_json = json.loads(fire_response)['data']
     queue = base.get_processed_comments()
     for comment in fire_json:
@@ -81,33 +125,30 @@ def stream_comments():
             fixed_fullname = fullname
         try:
             if fixed_fullname not in queue:
-                praw_comment = reddit.comment(fullname).parent()
                 actual_comment = reddit.comment(fullname)
-                if "!redditdiamond" in str(actual_comment.body).lower():
-                    author_parent = str(praw_comment.author)
-                    parent_id = comment['parent_id']
-                    instance_type = str(type(praw_comment))
-                    print("TYPE OF INSTANCE: " + instance_type)
-                    if 'Submission' in instance_type:
-                        parent_comment = praw_comment.selftext
-                        print("DETECTED SUBMISSION. SELFTEXT = " + praw_comment.selftext)
-                    elif 'Comment' in instance_type:
-                        parent_comment = praw_comment.body
-                        print("Detected comment")
-                    else:
-                        print('FATAL ERROR in processing comment parent:' + fullname, parent_id)
-                    author = comment['author']
-                    parent = comment['parent_id']
-                    subreddit = comment['subreddit']
-                    permalink = comment['permalink']
-                    print("[stream] add_diamond: " + author_parent + " " + author + " " + fullname + " " + subreddit + "\n" + parent_comment)
-                    new_diamond = base.add_diamond(author_parent, author, fullname, subreddit, parent_comment, permalink)
-                    print(new_diamond)
-                    reply = formatter.initial_comment(str(author_parent), str(actual_comment.author), new_diamond)
-                    print(reply)
-                    actual_comment.reply(reply)
-                    base.set_comment_as_processed(fullname)
+                praw_comment = actual_comment.parent()
+                author_parent = str(praw_comment.author)
+                parent_id = comment['parent_id']
+                instance_type = str(type(praw_comment))
+                print("TYPE OF INSTANCE: " + instance_type)
+                if 'Submission' in instance_type:
+                    parent_comment = praw_comment.selftext
+                elif 'Comment' in instance_type:
+                    parent_comment = praw_comment.body
+                author = comment['author']
+                parent = comment['parent_id']
+                subreddit = comment['subreddit']
+                permalink = comment['permalink']
+                print("[stream] add_diamond: " + author_parent + " " + author + " " + fullname + " " + subreddit + "\n" + parent_comment)
+                new_diamond = base.add_diamond(author_parent, author, fullname, subreddit, parent_comment, permalink)
+                reply = formatter.initial_comment(str(author_parent), str(actual_comment.author), new_diamond)
+                actual_comment.reply(reply)
+                print(reply)
+                base.set_comment_as_processed(fullname)
+                print('Comment marked as processed.')
+                return True
         except (KeyError, praw.exceptions.APIException) as e:
+            return False
             print(e)
 
 
@@ -129,66 +170,3 @@ def diamondSuccess_API(amount, donator, code, paypal_url, charity):
     print("diamondSuccess_API:", donator)
     theperson = reddit.redditor(donator).message('Diamond ' + str(code) + ' Claimed!', message_body)
     return verified_diamond["owner"]
-
-
-@app.route('/')
-def root():
-    abort(500, "No Request Specified")
-
-
-@app.route('/<name>')
-def func_proc(name):
-
-    if "poll" in name:
-        stream_comments()
-        base.ticker = base.ticker + 1
-        if base.ticker > 180:
-            print("Requesting new token..")
-            base.refresh_token()
-            base.ticker = 0
-        return "Polled"
-
-    elif "status" in name:
-        status_json = jsonify({'status': str(base.status)})
-        return status_json
-
-    else:
-        # action API with optional arguments
-        action = request.args.get('action', default='invalid', type=str)
-
-        if "validate" in action:
-            code = request.args.get('code', default='invalid', type=str)
-            transaction = request.args.get('transaction', default='http://invalid', type=str)
-            donator = request.args.get('donator', default='invalid', type=str)
-
-            print("Init API:", donator)
-
-            if "override" in transaction:
-                amount = randint(1,15)
-                charity = random.choice(charities)
-
-            else:
-                try:
-                    amount, charity = get_receipt_info(transaction)
-
-                except:
-                    jsonret = jsonify({'ERROR': 'Failed to authenticate Paypal receipt'})
-                    abort(500, jsonret)
-
-            if amount>0:
-                print("Success")
-                owner = diamondSuccess_API(amount,donator,code,transaction, charity)
-                jsonret = jsonify({'amount': str(amount), 'charity': str(charity)})
-                print(jsonret)
-                return jsonret
-
-            else:
-                jsonret = jsonify({'ERROR': 'Failed to authenticate Paypal receipt'})
-                abort(500, jsonret)
-
-    # don't know what to do here
-    abort(500, jsonify({'ERROR': 'Unknown Request'}))
-
-
-if __name__ == '__main__':
-    app.run(use_reloader=False)
